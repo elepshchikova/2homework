4501d1b956cc91169c2060faf9417110914d60c5
elepshchikova <elepshchikova@mail.ru>
2022-09-09 16:51:33 +0300

    Третья задача

--- tasks123/Program.cs
+++ tasks123/Program.cs
@@ -3,14 +3,24 @@
 // System.Console.WriteLine(n/10%10);
 
 
+// // int n = new Random().Next(100,100000);
 
+// if (n < 100){
+// System.Console.WriteLine("Третьей цифры нет");
+// }
+// else{
+// int result = n / 100 % 10;
+// System.Console.WriteLine($"Третья цифра числа {n} -> {result}");
+// }
 
-int n = new Random().Next(100,10000);
+// //int day = 6;
+// if (day < 1 | day > 7){
+// System.Console.WriteLine("Неверный день недели");
+// }
+// if (day > 5 && day < 8){
+//     System.Console.WriteLine("Это выходной");
+// }
+// else{
+//     System.Console.WriteLine("Будни");
+// }
 
-if (n < 100){
-System.Console.WriteLine("Третьей цифры нет");
-}
-else{
-int result = n / 100 % 10;
-System.Console.WriteLine($"Третья цифра числа {n} -> {result}");
-}
