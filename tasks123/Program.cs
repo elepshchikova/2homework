df9a56ba279f82d551dfc45c9b24d16a7a2260b5
elepshchikova <elepshchikova@mail.ru>
2022-09-09 16:44:45 +0300

    Вторая задача

--- tasks123/Program.cs
+++ tasks123/Program.cs
@@ -1,5 +1,16 @@
-﻿int n = 918;
+﻿// // int n = 918;
 
-System.Console.WriteLine(n/10%10);
+// System.Console.WriteLine(n/10%10);
 
 
+
+
+int n = new Random().Next(100,10000);
+
+if (n < 100){
+System.Console.WriteLine("Третьей цифры нет");
+}
+else{
+int result = n / 100 % 10;
+System.Console.WriteLine($"Третья цифра числа {n} -> {result}");
+}
